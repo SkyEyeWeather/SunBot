d40e08819006e7833fb28abe289af5f0b83cb440
clement-pages <pagesclement31@gmail.com>
2023-04-13 19:21:58 +0200

    Achieve merge

--- sunbot/weather_event.py
+++ sunbot/weather_event.py
@@ -18,7 +18,6 @@ SERVER_SUB_TYPE = 's'
 SUB_TYPE_LIST = [USER_SUB_TYPE, SERVER_SUB_TYPE]
 SubType = Literal['u', 's']
 
-
 # One class for all locations in order to not have too many tasks running in the same time
 class WeatherEvent(ABC):
     """Abstract class representing a general weather event. Because this class
