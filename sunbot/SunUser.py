eacd765fa96d8028daf643c53e797d28f3d1a42f
clement-pages <pagesclement31@gmail.com>
2023-12-09 23:49:16 +0100

    remove: remove fun features
    These features were not used anymore, and complicates the code.

--- sunbot/SunUser.py
+++ sunbot/SunUser.py
@@ -15,26 +15,20 @@ import os
 
 class SunUser:
     """This class represents a SunBot user. Each user has an ID that allows to 
-    identify it in discord API. This can be used to send it a message for example. 
-    In this class, an user is also defined by its name.
+    identify it in discord API. This can be used to send it a message for example.
     """
 
     usr_backup_path = ""
 
-    def __init__(self, id : int, emoji : str = "", freqEmoji : float = 0.5, favLocation : str = "Toulouse", mp : bool = False) -> None:
+    def __init__(self, id : int, favLocation : str = "Toulouse", mp : bool = False) -> None:
         """Constructor for this class. A SunBot user is defined by its Discord ID. 
-        Other informations can be provided in arguments, such as an emoji that is 
-        used to react to a message sent by this user, if the user authorize private 
+        Other informations can be provided in arguments, such as if the user authorize private 
         message from the bot, or a specific favorite location for weather notifications. 
         Each instance of this class is associated to a backup file. The link 
         between the user and its backup file is done thanks to the user ID 
         (so ID is defined as a constant and cannot be modified).
         ## Parameters:
         * `id`: discord identifiant of the SunBot user to create
-        * `emoji`: optional, string code corresponding to the emoji that will be 
-        used to react to messages sent by the user
-        * `freqEmoji`: optional, float corresponding to frequency at which an emoji 
-        is added to a message from the user
         * `favLocation`: optional, string indicating the favourite location name 
         for user to create. Default to Toulouse
         * `mp`: optional, boolean indicating if the user allows private messages 
@@ -61,8 +55,6 @@ class SunUser:
             with open(f"{self.usr_backup_path}{id}.json", "r", encoding="UTF-8") as userFile:
                 try:
                     userData = json.load(userFile)
-                    object.__setattr__(self, "emoji", userData["emoji"])
-                    object.__setattr__(self, "freqEmoji", userData["freqEmoji"])
                     object.__setattr__(self, "favLocation", userData["favLocation"])
                     object.__setattr__(self, "mp", userData["mp"])
                 except json.decoder.JSONDecodeError:
@@ -83,12 +75,6 @@ class SunUser:
             #User id cannot be modified:
             if __name == "id":
                 logging.error("User identifiant cannot be modified. Abort")
-                return
-            #Value for emoji frequency must be in [0, 1] interval:
-            if __name == "freqEmoji":
-                if __value < 0 or __value > 1:
-                    raise ValueError
-                object.__setattr__(self, __name, __value)
             else:
                 object.__setattr__(self, __name, __value)
             self.save_usr_data()
